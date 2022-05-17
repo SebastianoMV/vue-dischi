d4dc47362838fb0ddb10547dd3b7374f3cfa2d3c
SebastianoMV <sebastianovisco@gmail.com>
2022-05-17 17:21:14 +0200

    Esercizio completo senza bonus

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,35 +1,44 @@
 <template>
-<div>
-  cards
+<div class="col my-3">
+  <div class="smv-card d-flex flex-column  align-items-center p-2 text-center">
+    <img :src="card.poster" :alt="card.poster" class="mt-2">
+    <h4 class="my-2">{{card.title}}</h4>
+    <p>{{card.author}}</p>
+    <p>{{card.year}}</p>
+
+  </div>
+  
 </div>
   
 </template>
 
 <script>
-import axios from 'axios';
+
 
 export default {
   name:'CardComp',
-  data(){
-    return{
-      apiUrl:'https://flynn.boolean.careers/exercises/api/array/music'
-    }
-  },
-  mounted() {
-    this.getApi();
-  },
-  methods: {
-    getApi(){
-      axios.get(this.apiUrl)
-      .then(response => {
-        console.log(response.data);
-      })
-    }
-  },
+  props:{
+    card: Object
+  }
 
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/var';
+.smv-card{
+  width: 100%;
+  height: 400px;
+  background-color: $primary-color;
+  img{
+    width: 90%;
+  }
+  h4{
+    color: white;
+  }
+  p{
+    color: gray;
+  }
+}
 
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,7 +1,10 @@
 <template>
 <main >
-  <div class="container">
-    <CardComp/>
+  <div class="container p-5">
+    <div class="row row-cols-5">
+      <CardComp v-for="(card, index) in musicCards.response" :key="index" :card='card'/>
+    </div>
+    
   </div>
   
 </main>
@@ -10,15 +13,37 @@
 
 <script>
 import CardComp from './CardComp.vue';
+import axios from 'axios';
+
 export default {
     name: "MainComp",
-    components: { CardComp }
+    components: { CardComp },
+
+    data(){
+      return{
+       apiUrl:'https://flynn.boolean.careers/exercises/api/array/music',
+       musicCards:[]
+      }
+    },
+    mounted() {
+      this.getApi();
+    },
+    methods: {
+      getApi(){
+        axios.get(this.apiUrl)
+        .then(response => {
+          console.log(response.data);
+          this.musicCards = response.data;
+          console.log(this.musicCards);
+        })
+     }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 main{
-  height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px);
   background-color: #1e2d3b;
 }
 
