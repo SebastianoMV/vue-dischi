1beffee7e35f8c697838c1fd7869cef7aca184b8
SebastianoMV <sebastianovisco@gmail.com>
2022-05-17 17:35:51 +0200

    completo con bonus

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,10 +1,13 @@
 <template>
 <main >
-  <div class="container p-5">
+  <div class="container p-5" v-if="loaded">
     <div class="row row-cols-5">
       <CardComp v-for="(card, index) in musicCards.response" :key="index" :card='card'/>
     </div>
     
+  </div>
+  <div v-else class="d-flex justify-content-center align-items-center">
+    <div class="lds-ripple"><div></div><div></div></div>
   </div>
   
 </main>
@@ -21,6 +24,7 @@ export default {
 
     data(){
       return{
+       loaded: false,
        apiUrl:'https://flynn.boolean.careers/exercises/api/array/music',
        musicCards:[]
       }
@@ -35,6 +39,7 @@ export default {
           console.log(response.data);
           this.musicCards = response.data;
           console.log(this.musicCards);
+          this.loaded=true;
         })
      }
     },
@@ -45,6 +50,56 @@ export default {
 main{
   min-height: calc(100vh - 50px);
   background-color: #1e2d3b;
+  .d-flex{
+    height: calc(100vh - 50px);
+  }
 }
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid #dfc;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  4.9% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  5% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+
 
 </style>
